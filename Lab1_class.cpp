2f0fb5d180b2ac4a6cf94be4cb7b53ae5263bb96
Irina Stepanova <110852608+Irissha@users.noreply.github.com>
2022-11-16 10:46:12 +0500

    Update Lab1_class.cpp

--- Lab1_class.cpp
+++ Lab1_class.cpp
@@ -52,3 +52,38 @@ int main()
 
     return 0;
 }
+*/Header.h*/
+/*#pragma once   
+class money
+{
+private:
+    int first;
+    int second;
+public:
+    int summa();
+
+    void Display();
+
+    void Read(int nom);
+    friend class CashIn;
+};
+
+class CashIn
+{
+private:
+    static const int K = 9;
+    int allowed[K];
+    money a[K];
+
+public:
+    void ReadAll();
+    void DisplayAll();
+    int TotalSum();
+
+    CashIn() 
+    {
+        int t[K] = { 1,2,5,10,50,100,500,1000,5000 };
+        for (int j = 0; j < K; j++)
+            allowed[j] = t[j];
+    }
+}; */
